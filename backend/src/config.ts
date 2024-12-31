a055ce7743262da95f21a4130d142b6cece69bd9
PomPerWoW <chatthanes22@gmail.com>
2025-01-01 02:08:29 +0700

    add script to package.json and modify tsconfig

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -5,12 +5,10 @@ dotenv.config({});
 export class Config {
   public NODE_ENV: string | undefined;
   public SERVER_PORT: string | undefined;
-  public PINO_LOG_LEVEL: string | undefined;
 
   constructor() {
     this.NODE_ENV = process.env.NODE_ENV || '';
     this.SERVER_PORT = process.env.SERVER_PORT || '';
-    this.PINO_LOG_LEVEL = process.env.PINO_LOG_LEVEL || '';
   }
 }
 
--- backend/src/logger.ts
+++ backend/src/logger.ts
@@ -1,8 +1,14 @@
 import pino, { Logger } from 'pino';
-import config from '@piggy/config';
 
 const logger: Logger = pino({
-  level: config.PINO_LOG_LEVEL || 'info',
+  transport: {
+    targets: [
+      {
+        target: 'pino-pretty',
+        options: { colorize: true },
+      },
+    ],
+  },
 });
 
 export default logger;
